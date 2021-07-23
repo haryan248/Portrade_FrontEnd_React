faee8abf95e9fffbf326136a4b786852376e0638
aejin24 <aejin24@naver.com>
2021-07-23 15:15:43 +0900

    불필요한 파일 제거

--- src/components/home/index.js
+++
@@ -1,5 +0,0 @@
-export { default as Introduce } from "./Introduce";
-export { default as  Notice } from "./Notice";
-export { default as Thumbnail } from "./Thumbnail";
-export { default as Review } from "./Review";
-export { default as Recommand } from "./Recommand";
