4cd5a661b643d2dca87cb1f0047fca7279687fac
haryan248 <haryan96@naver.com>
2021-07-29 15:06:25 +0900

    사이트 소개 페이지 레이아웃 구성

--- src/pages/Introduce.js
+++ src/pages/Introduce.js
@@ -1,9 +1,70 @@
 import React from "react";
+import "./css/introduce.css";
 
 const Introduce = () => {
     return (
-        <div style={{width: "100%", textAlign: "center"}}>
-            <h1>Introduce</h1>
+        <div className="introduce-container">
+            <div className="introduce-box">이미지</div>
+
+            <div className="intro-box">
+                <div className="intro-content">
+                    <div className="first_intro-desc">
+                        <div>
+                            <span className="focus">PORTRADE</span>는 당신과 기업의 연결고리 입니다.
+                        </div>
+                    </div>
+                </div>
+                <div className="intro-content">
+                    <div className="second_intro-desc">
+                        <div>
+                            코로나 19로 인해, 취업 한파가 장기화되며 취직이 어려운 지금, 포트레이트는 디자이너에게 한정된 기존 포트폴리오 사이트를 개선하여, 분야에 상관 없이 누구나 포트폴리오를 자유롭개 공유할 수 있는 공간입니다.
+                            포트레이트는 디자인에 국한된 한계점을 넘어서 모든 분야의 사람들이 포트폴리오를 업로드 할 수 있습니다.
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div className="vision-box">
+                <div className="vision-content">
+                    <div className="first_vision-desc">
+                        <div>로고</div>
+                    </div>
+                </div>
+                <div className="vision-content">
+                    <div className="second_vision-desc">
+                        <div className="vision-title">포트레이트의 비전</div>
+                        <div>포트레이트는 디자이너에 국한된 기존의 사이트에서 벗어나, 다양한 청년들의 가능성을 발굴합니다. 다양한 분야의 포트폴리오 등록의 기회를 제공하며 성장의 여정을 함께합니다.</div>
+                        <div>포트레이트는 디자이너에 국한된 기존의 사이트에서 벗어나, 다양한 청년들의 가능성을 발굴합니다. 다양한 분야의 포트폴리오 등록의 기회를 제공하며 성장의 여정을 함께합니다.</div>
+                    </div>
+                </div>
+            </div>
+
+            <div className="catch-box">
+                <div className="catch-content">
+                    <div className="catch-desc">
+                        <div className="catch-title">가치 있는 포트폴리오를, 더 나은 전시회로</div>
+                        <div clasName="catch-detail">
+                            포트레이트는 디자이너에 국한된 기존의 사이트에서 벗어나, 다양한 청년들의 가능성을 발굴합니다. 다양한 분야의 포트폴리오 등록의 기회를 제공하며 성장의 여정을 함께합니다. 포트레이트는 디자이너에 국한된 기존의
+                            사이트에서 벗어나, 다양한 청년들의 가능성을 발굴합니다.다양한 분야의 포트폴리오 등록의 기회를 제공하며 성장의 여정을 함께합니다. 포트레이트는 디자이너에 국한된 기존의 사이트에서 벗어나, 다양한 청년들의 가능성을
+                            발굴합니다. 다양한 분야의 포트폴리오 등록의 기회를 제공하며 성장의 여정을 함께합니다.
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="linkerbell-box">
+                <div className="linkerbell-title">Linker Bell</div>
+                <div className="linekerbell-intro">포트레이트를 제작한 개발자 입니다.</div>
+                <div className="linekerbell-desc">
+                    사이트에 문제가 발생 할 시, 피터팬의 팅커벨처럼 여러분들을 바로 도와줄 수 있는 개발자입니다. 저희 링커벨은 취업준비생의 열정이 담긴 초상화를 더 넓은 무대에서 전시할 수 있도록 도와드립니다.
+                </div>
+                <div className="linekerbell-desc">포트레이트는 파일 업로드를 통하여 간편한 등록이 가능합니다. 여러분의 개성넘치는 포트폴리오를 업로드 해보세요.</div>
+            </div>
+
+            <div className="developer-container">
+                <div className="developer"></div>
+                <div className="developer"></div>
+                <div className="developer"></div>
+            </div>
         </div>
     );
 };
