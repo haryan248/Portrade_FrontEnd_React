32109f1f55959ffd49c8f9092ab08e8a34b7300d
haryan248 <haryan96@naver.com>
2021-07-31 19:51:56 +0900

    마이페이지 레이아웃 구성

--- src/pages/Mypage.js
+++ src/pages/Mypage.js
@@ -1,9 +1,51 @@
 import React from "react";
+import "./css/my-page.css";
 
 const Mypage = () => {
     return (
-        <div style={{width: "100%", textAlign: "center"}}>
-            <h1>Mypage</h1>
+        <div className="myPage-container">
+            <div class="ProfileBanner-root Profile-banner">
+                <div className="ProfileBanner-promptContainer">
+                    <div className="ProfileBanner-prompt"></div>
+                </div>
+            </div>
+            <div className="Profile-root">
+                <div className="Profile-wrap">
+                    <div className="ProfileCard-root">
+                        <div className="ProfileCard-header">
+                            <div className="Avatar-container">
+                                <div className="ProfileAddStoryButton-container">
+                                    <div className="AddStoryButton-addStoryIcon"></div>
+                                </div>
+                            </div>
+                            <h1 className="ProfileCard-userFullName">홍길동</h1>
+                            <div className="ProfileCard-userDetails">사용자 소속 및 구직 상태</div>
+                        </div>
+
+                        <div className="ProfileCard-userInteractions">
+                            <div className="ProfileCard-interactionButton ProfileCard-editButton">
+                                <div>
+                                    <div className="ProfileCard-buttonLabel">내 프로필 편집</div>
+                                </div>
+                            </div>
+                            <div className="ProfileCard-interactionButton">
+                                <div className="ProfileCard-buttonLabel">
+                                    <div> 구직 상태 편집 </div>
+                                </div>
+                            </div>
+                            <div className="ProfileCard-interactionButton">
+                                <div className="ProfileCard-buttonLabel">
+                                    <div>포트폴리오 관리 </div>
+                                </div>
+                            </div>
+                            <div className="ProfileCard-interactionButton">
+                                <div className="ProfileCard-signUpDate">멤버 가입일 : 2021년 1월 1일</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="Profile-profileContents-3cP"></div>
+            </div>
         </div>
     );
 };
