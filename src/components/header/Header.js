1a9656f1491d3f1c5c9b4ca85b316c2571bc697b
aejin24 <aejin24@naver.com>
2021-07-31 17:07:32 +0900

    remove unused hooks

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
 import ActionCreators from "../../_actions";
 import { useDispatch,useSelector } from "react-redux";
