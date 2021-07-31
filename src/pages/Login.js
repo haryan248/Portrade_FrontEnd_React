8ca45c4a6364cba1752359f5b5ffa3fde3cdd603
haryan248 <haryan96@naver.com>
2021-07-31 19:58:43 +0900

    정렬 수정

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -13,11 +13,11 @@ const Login = ({ history }) => {
         dispatch(ActionCreators.login(res.isSignedIn(), res.Zb.access_token, res.profileObj.email));
         window.sessionStorage.setItem("access_token", res.Zb.access_token);
         history.replace("/");
-    }
+    };
 
     const onFailureGoogle = (err) => {
         console.log(err);
-    }
+    };
 
     return (
         <div style={{ width: "100%", textAlign: "center" }}>
@@ -32,26 +32,14 @@ const Login = ({ history }) => {
                         </div>
 
                         <div className="slogan-text">
-                            <p>
-                                다양한 분야의 포트폴리오를 업로드 할 수 있는
-                                공간입니다.
-                            </p>
-                            <p>
-                                취업난 속에서 기업과 청년의 연결을 도모합니다.
-                            </p>
-                            <p>
-                                다양한 분야의 포트폴리오를 업로드 할 수 있는
-                                공간입니다.
-                            </p>
-                            <p>
-                                취업난 속에서 기업과 청년의 연결을 도모합니다.
-                            </p>
+                            <p>다양한 분야의 포트폴리오를 업로드 할 수 있는 공간입니다.</p>
+                            <p>취업난 속에서 기업과 청년의 연결을 도모합니다.</p>
+                            <p>다양한 분야의 포트폴리오를 업로드 할 수 있는 공간입니다.</p>
+                            <p>취업난 속에서 기업과 청년의 연결을 도모합니다.</p>
                         </div>
 
                         <div className="more-info-text">
-                            <Link to="/introduce">
-                                플랫폼 더 알아보기 {">"}
-                            </Link>
+                            <Link to="/introduce">플랫폼 더 알아보기 {">"}</Link>
                         </div>
 
                         <Link to="/register" className="btn-register">
@@ -63,18 +51,15 @@ const Login = ({ history }) => {
                     <div className="login-content">
                         <div className="login-text">로그인</div>
                         <div className="login-intro">
-                            <div>
-                                포트레이트의 회원이 되시면, 포트폴리오 등록 및
-                                기업 공고 열람
-                            </div>
+                            <div>포트레이트의 회원이 되시면, 포트폴리오 등록 및 기업 공고 열람</div>
                             <div>서비스를 간편하게 이용하실 수 있습니다.</div>
                         </div>
 
                         <div className="btn-oauth">
                             <GoogleLogin
                                 clientId="988674118538-gog8quj6fimubp2b8dc6jhecui5e88cd.apps.googleusercontent.com" //앱키 추후 변경
-                                onSuccess={res => onSuccessGoogle(res)}
-                                onFailure={err => onFailureGoogle(err)}
+                                onSuccess={(res) => onSuccessGoogle(res)}
+                                onFailure={(err) => onFailureGoogle(err)}
                                 cookiePolicy="none"
                             />
                         </div>
@@ -87,10 +72,7 @@ const Login = ({ history }) => {
                             </p>
                             비밀번호
                             <p>
-                                <input
-                                    className="login-input"
-                                    type="password"
-                                />
+                                <input className="login-input" type="password" />
                             </p>
                         </div>
 
@@ -101,9 +83,7 @@ const Login = ({ history }) => {
                                 <input className="checkbox" type="checkbox" />
                                 아이디 / 비밀번호 저장
                             </div>
-                            <div className="find-passwd">
-                                비밀번호를 잊으셨나요?
-                            </div>
+                            <div className="find-passwd">비밀번호를 잊으셨나요?</div>
                         </div>
                     </div>
                 </div>
