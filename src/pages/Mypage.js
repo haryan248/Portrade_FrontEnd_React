ebcc42863c82eca886ab051c15a19c11566cb820
haryan248 <haryan96@naver.com>
2021-08-01 14:41:44 +0900

    마이페이지 레이아웃 구현

--- src/pages/Mypage.js
+++ src/pages/Mypage.js
@@ -1,16 +1,18 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./css/my-page.css";
 
 const Mypage = () => {
     return (
         <div className="myPage-container">
-            <div class="ProfileBanner-root Profile-banner">
+            <div className="ProfileBanner-root Profile-banner">
                 <div className="ProfileBanner-promptContainer">
                     <div className="ProfileBanner-prompt"></div>
                 </div>
             </div>
             <div className="Profile-root">
                 <div className="Profile-wrap">
+                    {/* 프로필 카드 */}
                     <div className="ProfileCard-root">
                         <div className="ProfileCard-header">
                             <div className="Avatar-container">
@@ -23,7 +25,7 @@ const Mypage = () => {
                         </div>
 
                         <div className="ProfileCard-userInteractions">
-                            <div className="ProfileCard-interactionButton ProfileCard-editButton">
+                            <div className="ProfileCard-interactionButton">
                                 <div>
                                     <div className="ProfileCard-buttonLabel">내 프로필 편집</div>
                                 </div>
@@ -43,8 +45,66 @@ const Mypage = () => {
                             </div>
                         </div>
                     </div>
+                    {/* 프로필 컨텐츠 */}
+                    <div className="Profile-profileContents">
+                        <ul className="Profile-tabControl Profile-tabsWithBannerWithoutFeatures">
+                            <li className="Profile-tab">
+                                <Link to="" className="Profile-anchor">
+                                    포트폴리오 등록
+                                </Link>
+                            </li>
+                            <li className="Profile-tab">
+                                <Link to="" className="Profile-anchor">
+                                    나의 포트폴리오
+                                </Link>
+                            </li>
+                            <li className="Profile-tab">
+                                <Link to="" className="Profile-anchor">
+                                    평가
+                                </Link>
+                            </li>
+                            <li className="Profile-tab">
+                                <Link to="" className="Profile-anchor">
+                                    저장된 항목
+                                </Link>
+                            </li>
+                            <li className="Profile-tab">
+                                <Link to="" className="Profile-anchor">
+                                    구독관리
+                                </Link>
+                            </li>
+                        </ul>
+                        <div className="Profile-tabs">
+                            <div className="Profile-tab">
+                                <div>
+                                    <div className="Work">
+                                        <div className="portfolioAddContent-info">
+                                            <div className="portfolioAddContent-card">
+                                                <div className="portfolioAddContent-wrapper">
+                                                    <div className="box">
+                                                        <div className="portfolioAddContent-card__item">
+                                                            <div className="portfolioAddContent-icon-wrapper">
+                                                                <div className="portfolioAddContent-icon"></div>
+                                                            </div>
+                                                            <div className="addPortfolio-wrapper">
+                                                                <div className="addPortfolio-button">
+                                                                    <div className="addPortfolio">포트폴리오 등록</div>
+                                                                </div>
+                                                            </div>
+                                                            <p className="paragraph">
+                                                                피드백, 조회수 및 평가를 받아 보십시오.<br></br> 공개 프로젝트는 당사의 큐레이터가 추천해 드릴 수 있습니다.
+                                                            </p>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <div className="Profile-profileContents-3cP"></div>
             </div>
         </div>
     );
