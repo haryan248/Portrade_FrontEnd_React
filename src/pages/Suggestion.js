5d4c67500cf4bb383eb1c6fd497340d2aba03054
GyuSeon-Jo <plame114@gmail.com>
2021-08-08 18:24:46 +0900

    Suggestion css명 통일

--- src/pages/Suggestion.js
+++ src/pages/Suggestion.js
@@ -8,15 +8,15 @@ const Suggestion = () => {
                 <p>추천 기업입니다.</p>
                 <p>
                     취업의 기회를 여러분에게
-                    <br/>
+                    <br />
                     포트레이트에서 간편하게 기업 공고를 열람해 보세요.
                 </p>
                 <p>자세히 보기</p>
             </div>
-            <div className="company-list">
-                <div className="company"></div>
-                <div className="focus-company"></div>
-                <div className="company"></div>
+            <div className="suggestion-company-list">
+                <div className="company">추천 기업 예시</div>
+                <div className="focus-company">추천 기업 예시</div>
+                <div className="company">추천 기업 예시</div>
             </div>
         </div>
     );
