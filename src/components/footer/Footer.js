ac48ed0d128e84b26026124bd7c626d1fc997803
GyuSeon-Jo <plame114@gmail.com>
2021-08-08 18:22:33 +0900

    footer 레잉아웃 조정

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -6,16 +6,27 @@ import "./css/footer.css";
 const Footer = () => {
     return (
         <footer>
-            <div className="footer-logo">
-                <Link to="/">PORTRADE</Link>
+            <div className="footer-wrap">
+                <div className="footer-logo">
+                    <Link to="/">PORTRADE</Link>
+                    <nav className="footer-nav-bar">
+                        <Link to="">포트레이드 소개</Link>
+                        <Link to="">서비스 소개</Link>
+                        <Link to="">공지사항</Link>
+                        <Link to="">자주 묻는 질문</Link>
+                        <Link to="">문의하기</Link>
+                    </nav>
+                </div>
+                <div className="footer-contact">
+                    <p>CONTACT</p>
+                    <div className="footer-contact-box-wrap">
+                        <div className="footer-contact-box"></div>
+                        <div className="footer-contact-box"></div>
+                        <div className="footer-contact-box"></div>
+                        <div className="footer-contact-box"></div>
+                    </div>
+                </div>
             </div>
-            <nav className="footer-nav-bar">
-                <Link to="">포트레이드 소개</Link>
-                <Link to="">서비스 소개</Link>
-                <Link to="">공지사항</Link>
-                <Link to="">자주 묻는 질문</Link>
-                <Link to="">문의하기</Link>
-            </nav>
         </footer>
     );
 };
