9a9cd97a07796cd51604cc31c00fa773ee43f897
Hyun.Ha <51049245+haryan248@users.noreply.github.com>
2021-09-02 21:10:32 +0900

    로그인 post 기능 테스트

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -3,16 +3,27 @@ import { Link } from "react-router-dom";
 import GoogleLogin from "react-google-login";
 import ActionCreators from "../_actions";
 import { useDispatch } from "react-redux";
+import Axios from "axios";
 
 import "./css/login.css";
 
 const Login = ({ history }) => {
     const dispatch = useDispatch();
 
+    const config = {
+        headers: {
+            "Content-Type": "application/json; charset=utf-8",
+        },
+    };
+
     const onSuccessGoogle = (res) => {
-        dispatch(ActionCreators.login(res.isSignedIn(), res.Zb.access_token, res.profileObj.email));
-        window.sessionStorage.setItem("access_token", res.Zb.access_token);
-        history.replace("/");
+        // dispatch(ActionCreators.login(res.isSignedIn(), res.Zb.access_token, res.profileObj.email));
+        // window.sessionStorage.setItem("access_token", res.Zb.access_token);
+        let jwtToken = await Axios.post("http://ec2-3-38-6-25.ap-northeast-2.compute.amazonaws.com:8080/oauth/jwt/google", JSON.stringify(res), config);
+        if (jwtToken.status === 200) {
+            localStorage.setItem("jwtToken", jwtToken.data);
+            history.replace("/");
+        }
     };
 
     const onFailureGoogle = (err) => {
@@ -22,7 +33,9 @@ const Login = ({ history }) => {
     return (
         <div className="login-box">
             <div className="login-left-box">
-                <Link to="/" className="login-logo-text">PORTRADE</Link>
+                <Link to="/" className="login-logo-text">
+                    PORTRADE
+                </Link>
                 <div className="login-intro-text">
                     <p>PORTRADE와 함께</p>
                     <p>취업을 향한 여정을 시작해보세요.</p>
@@ -39,7 +52,9 @@ const Login = ({ history }) => {
                     <Link to="/introduce">플랫폼 더 알아보기 &gt;</Link>
                 </div>
 
-                <Link to="/register" className="login-btn-register">회원가입</Link>
+                <Link to="/register" className="login-btn-register">
+                    회원가입
+                </Link>
             </div>
 
             <div className="login-right-box">
@@ -48,7 +63,7 @@ const Login = ({ history }) => {
                     <div>포트레이트의 회원이 되시면, 포트폴리오 등록 및 기업 공고 열람</div>
                     <div>서비스를 간편하게 이용하실 수 있습니다.</div>
                 </div>
-                
+
                 <GoogleLogin
                     clientId="988674118538-gog8quj6fimubp2b8dc6jhecui5e88cd.apps.googleusercontent.com" //앱키 추후 변경
                     onSuccess={(res) => onSuccessGoogle(res)}
@@ -75,8 +90,10 @@ const Login = ({ history }) => {
 
                 <div className="login-option">
                     <div>
-                        <input type="checkbox" id="login-info-save"/>
-                        <label htmlFor="login-info-save" className="login-info-save">아이디 / 비밀번호 저장</label>
+                        <input type="checkbox" id="login-info-save" />
+                        <label htmlFor="login-info-save" className="login-info-save">
+                            아이디 / 비밀번호 저장
+                        </label>
                     </div>
 
                     <p className="login-find-pw">비밀번호를 잊으셨나요?</p>
